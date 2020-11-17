6f5564648aa9200ab9f8d9256d080eba6b930890
Taif Rahaman <taif.rahaman73@gmail.com>
2020-11-18 02:09:22 +0600

    header section added

---
+++ src/components/header.js
@@ -0,0 +1,51 @@
+import { Link } from "gatsby"
+import React, { useEffect } from "react"
+
+import { Container, Flex } from "../styles/GlobalStyle"
+import { HeaderNav, Logo, Menu } from "../styles/headerStyle"
+
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+
+const Header = () => {
+  const dispatch = useGlobalDispatchContext()
+  const { currentTheme } = useGlobalStateContext()
+
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" })
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" })
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      window.localStorage.setItem("theme", currentTheme)
+    }
+  }, [currentTheme])
+
+  return (
+    <HeaderNav animate={{ y: 0, opacity: 1 }} initial={{ y: -72, opacity: 0 }}>
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo>
+            <Link to="/">FURR</Link>
+            <span onClick={toggleTheme}></span>
+            <Link to="/">W</Link>
+          </Logo>
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useState } from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+import Header from "./header"
+
+import { useGlobalStateContext } from "../context/globalContext"
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  *{
+    text-decoration: none;
+    /* cursor: none; */
+  }
+
+
+  html{
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+
+  body{
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden
+  }
+`
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const darkTheme = {
+    background: "#000",
+    text: "#fff",
+    red: "#ea291e",
+  }
+
+  const lightTheme = {
+    background: "#fff",
+    text: "#000",
+    red: "#ea291e",
+  }
+
+  const { currentTheme } = useGlobalStateContext()
+
+  return (
+    <ThemeProvider theme={currentTheme === "dark" ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Header />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
+}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
